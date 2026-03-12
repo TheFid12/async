a1f22a9333c2c55849e116a46b30ce8af3e08610
TheFid12 <zhenya.bytchkov@mail.ru>
2026-03-12 22:13:50 +0300

    modify laba1 code

--- src/main.py
+++ src/main.py
@@ -1,37 +1,25 @@
-from loguru import logger
-from src.source import (
-    FileTaskSource,
-    GeneratorTaskSource,
-    APITaskSource,
-    collect_all,
-)
-
-logger.remove()
-logger.add(
-    "log.txt",
-    format="{time:HH:mm:ss} - {level} - {message}",
-    level="INFO",
-    mode="a",
-    encoding="utf-8",
-)
-
+import logging
+from src.source import FileSource, GeneratorSource, ApiSource, collect_all
 
 def main() -> None:
     """
-    Точка входа в модуль.
-    В sources указываются источники задач.
+    Запуск сборщика задач, который запрашивает и выполняет задачи
+    из различных источников.
     """
-    logger.info("Начало получения задач из источников.")
-    sources = [
-        FileTaskSource("tasks.json"),
-        GeneratorTaskSource(count=3, prefix="task"),
-        APITaskSource([{"id": "api_task_1", "payload": "from api"}])
-    ]
-    tasks = collect_all(sources)
-    logger.info(f"Сбор задач завершен. Всего: {len(tasks)}")
-    for task in tasks:
-        print(f"[{task.id}] {task.payload}")
+    logging.basicConfig(
+        filename="log.txt",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        encoding="utf-8"
+    )
 
+    sources = [
+        GeneratorSource(count=3),
+        FileSource("tasks.json"),
+        ApiSource([{"payload": "api data"}])
+    ]
+    
+    collect_all(sources)
 
 if __name__ == "__main__":
     main()
